a5a08b4d08e867e2c8c51112de3fec4b0182cfd3
AlmondChips <chips6t@gmail.com>
2022-07-24 16:23:51 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+	' ':' ',
 };
 
+const binaryToMorse = {
+	'10':'.',
+	'11':'-',
+	'00':'',
+	
+}
+
 function decode(expr) {
-    // write your solution here
+    const exprLength = expr.length;
+	const attemptCount = exprLength/10;
+
+	let result = '';
+
+	 for (let i = 0, pointer10 = 10; i < attemptCount; i++,pointer10+=10){
+		const binaryLetter = expr.substring(i*10, pointer10)
+	let letter = '';
+	
+		for (let j = 0, pointer2 = 2; j < 5; j++, pointer2+=2){
+			if (binaryLetter === "**********"){
+				letter+= " ";
+				break;
+			}
+			letter+= decodeBinary(binaryLetter.substring(j*2, pointer2))
+		}
+		result+= MORSE_TABLE[letter];
+	 }
+
+	 return result;
+	
+}
+
+function decodeBinary(binary){
+return binaryToMorse[binary];
 }
 
 module.exports = {
